c210503b92a6824a9a7026865bd868b38912ffdb
孙楷 (Kai) <ksun0017@qq.com>
2020-12-06 15:34:02 +1100

    Initial Commit

---
+++ Shared/heritage_online_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  heritage_online_SwiftUIApp.swift
+//  Shared
+//
+//  Created by 孙楷 on 6/12/20.
+//
+
+import SwiftUI
+
+@main
+struct heritage_online_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
